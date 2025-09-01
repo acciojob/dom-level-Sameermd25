8effbf953f3f4c29db2a0c275cf521afa75dcfd2
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-09-02 00:00:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const target = document.getElementById("level");
+
+let count = 0;
+let current = target;
+
+// Traverse up the DOM until there are no more parents
+while (current) {
+    count++;
+    current = current.parentElement;
+}
+
+alert(`The level of the element is: ${count}`)
